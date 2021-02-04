ba9d6293c8d1ff8c6b65ff9fad77f08cc7ef03b1
Bhupinder Singh <bhupindersi82@gmail.com>
2021-02-04 14:59:03 +0530

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use NumberToWords\NumberToWords;
+
+class HomeController extends Controller
+{
+    //
+    function list()
+    {
+        return view('home');
+    }
+
+    function cheque(Request $request)
+    {
+        $amount = $request['automation-amount'];
+        $numberToWords = new NumberToWords();
+
+        // build a new number transformer using the RFC 3066 language identifier
+        $numberTransformer = $numberToWords->getNumberTransformer('en');
+        $words = ucwords($numberTransformer->toWords($amount));
+        return view('cheque',['amount' => $amount, 'words' => $words]);
+    }
+}
