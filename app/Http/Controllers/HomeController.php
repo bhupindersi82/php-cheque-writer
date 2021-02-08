3918024a0ced631dc6e23eb7ab4d41f6df002516
Bhupinder Singh <bhupinder.singh@netsolutions.com>
2021-02-08 21:17:47 +0530

    decimal fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,13 +15,26 @@ class HomeController extends Controller
 
     function cheque(Request $request)
     {
-        $amount = $request['automation-amount'];
+        $n = $request['automation-amount'];
+        $amount = floor($n);
+        $decimal = number_format($n-$amount,2);
         $numberToWords = new NumberToWords();
 
         // build a new number transformer using the RFC 3066 language identifier
         $numberTransformer = $numberToWords->getNumberTransformer('en');
         $words = strtolower($numberTransformer->toWords($amount));
         $words = ucwords(str_replace('-',' ',$words));
-        return view('cheque',['amount' => $amount, 'words' => $words]);
+
+        if($decimal > 0){
+            $decimal = substr((string)$decimal,2);
+
+            $words .= " Point";
+            foreach (str_split($decimal) as $d) {
+                $words .= ' ' . ucwords(strtolower($numberTransformer->toWords($d)));
+            }
+        }
+        
+
+        return view('cheque',['amount' => $n, 'words' => $words]);
     }
 }
